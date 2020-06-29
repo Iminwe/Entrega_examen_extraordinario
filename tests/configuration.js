8333b9030fccad9290d5260d647ad5bc063df696
J. Fernando Sánchez <balkian@gmail.com>
2020-06-29 11:07:52 +0200

    Cambio asignación

--- tests/configuration.js
+++ tests/configuration.js
@@ -48,7 +48,7 @@ let modelos = read_models();
 
 
 function model_for_user (email) {
-    var id = 2;
+    var id = 10;
     for(const ix in email){
         id += 1;
         id += email.charCodeAt(ix);
